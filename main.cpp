88e0c690bffc0f758dff617675e14af9805edcc3
Saussereau Dominique <domsau2@yahoo.fr>
2019-05-17 19:59:08 +0200

    Remove unneeded /* */

--- main.cpp
+++ main.cpp
@@ -11,30 +11,6 @@ const int nbComposantRGB = 3;
 const int maxRGB = 256; // Each. // 65536 max.
 int rgb = 1; // R = 1, G = 2, B = 3.
 int imageTableauRGB_int[maxHz][maxVt][nbComposantRGB];
-/*
-void gotoxy(int x, int y) // https://codes-sources.commentcamarche.net/forum/affich-941809-remonter-au-ligne-precedentes-dans-une-console-win32
-{
-    COORD c = {x-1, y-1};
-    SetConsoleCursorPosition (GetStdHandle(STD_OUTPUT_HANDLE), c);
-}
-
-void delline() // https://codes-sources.commentcamarche.net/forum/affich-941809-remonter-au-ligne-precedentes-dans-une-console-win32
-{
-    COORD coord;
-    DWORD written;
-    CONSOLE_SCREEN_BUFFER_INFO info;
-
-    GetConsoleScreenBufferInfo (GetStdHandle (STD_OUTPUT_HANDLE),
-    &info);
-    coord.X = info.dwCursorPosition.X;
-    coord.Y = info.dwCursorPosition.Y;
-
-    FillConsoleOutputCharacter (GetStdHandle (STD_OUTPUT_HANDLE),
-    ' ', info.dwSize.X * info.dwCursorPosition.Y, coord, &written);
-    gotoxy (info.dwCursorPosition.X + 1,
-    info.dwCursorPosition.Y + 1);
-}
-*/
 
 int pixelInit ( int x, int y, int rgb )
 {
